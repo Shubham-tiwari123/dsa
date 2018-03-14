bb8096e8f1b4b8449950f2fafa48e59dadf8db23
Shubham-tiwari123 <st540362@gmail.com>
2018-03-14 22:33:52 +0530

    Array.h file

---
+++ Array.h
@@ -0,0 +1,17 @@
+#ifndef ARRAY_H
+#define ARRAY_H
+
+class Array{
+private:
+    int a[10];
+    int num;
+    int count=0;
+public:
+    void create();
+    void dispay();
+    void negativeLeftSide();
+    void seperateZeroOne();
+};
+
+#endif /* ARRAY_H */
+
