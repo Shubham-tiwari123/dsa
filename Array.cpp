c8f61753fb536f01b2cb943da896613595ff36fd
Shubham-tiwari123 <st540362@gmail.com>
2018-03-14 22:26:54 +0530

    Array cpp file

---
+++ Array.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <cstring>
+#include "Array.h"
+using namespace std;
+
+void Array::create(){
+    cout<<"Enter no of elements:-";
+    cin>>num;
+    for(int i=0;i<num;i++){
+        cout<<"\nEnter number:-";
+        cin>>a[i];
+    }
+}
+
+void Array::dispay(){
+    cout<<"\nDisplaying elements:-";
+    for(int i=0;i<num;i++){
+        cout<<a[i]<<"\t";
+    }
+}
+
+void Array::negativeLeftSide(){
+    int temp[num];
+    int j=0;
+    for(int i=0;i<num;i++){
+        if(a[i]>=0)
+            temp[j++]=a[i];
+    }
+    for(int i=0;i<num;i++){
+        if(a[i]<0)
+            temp[j++]=a[i];
+    }
+    memcpy(a,temp,sizeof(temp));
+}
+
+void Array::seperateZeroOne(){
+    for(int i=0;i<num;i++){
+        if(a[i]==0)
+            count++;
+    }
+    for(int i=0;i<count;i++)
+        a[i] = 0;
+    
+    for(int i=count;i<num;i++)
+        a[i] =1;
+}
